1fa4a65e4b8c666fe727dae01a59c7bb0b1816c8
ivan <ivan@DESKTOP-NELLSAH>
2023-10-25 20:19:26 +0300

    done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	const mainarr = bracketsConfig.map((Value) => Value.join(""));
+	for (let i = 0; i < mainarr.length; i++) {
+		if (str.includes(mainarr[i])) {
+			str = str.replace(mainarr[i], "");
+			console.log(str);
+			i = -1;
+		}
+	}
+	return !str;
+};
